1feeab44ae19c595eb852a6bce7b3eb98b4f516d
Igor Colin <igor@igor-Latitude-E7240.(none)>
2014-07-09 20:09:48 +0200

    add some openclassroom tutorial files

---
+++ crepes/blog/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+
+
+urlpatterns = patterns('blog.views',
+                       url(r'^accueil/$', 'home'),
+                       url(r'^article/(?P<id_article>\d+)/', 'view_article'),
+                       url(r'^articles/(?P<year>\d{4})/(?P<month>\d{2})/$', 'list_articles'),
+                       url(r'^redirection/$', 'view_redirection'),
+)
